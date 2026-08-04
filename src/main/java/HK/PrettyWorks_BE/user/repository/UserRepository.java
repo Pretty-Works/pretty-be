c8a0eaac20cfd3eba857bd149a91c89e8c863cd9
Kimsoomin <soomink.210@gmail.com>
2026-08-04 12:09:56 +0900

    fix : 에이전트 API v1 잔재로 인한 오류 수정(#95)

--- src/main/java/HK/PrettyWorks_BE/user/repository/UserRepository.java
+++ src/main/java/HK/PrettyWorks_BE/user/repository/UserRepository.java
@@ -2,11 +2,9 @@ package HK.PrettyWorks_BE.user.repository;
 
 import HK.PrettyWorks_BE.user.constant.StatusType;
 import HK.PrettyWorks_BE.user.domain.UserEntity;
-import org.springframework.data.domain.Pageable;
 import jakarta.persistence.LockModeType;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
